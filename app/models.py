fbbf82de07c12d05798bdf7e961e3ed574fdd4ad
nine96as <ashrafsaid79@gmail.com>
2024-01-09 17:11:39 +0000

    Create models.py with User model

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from .extensions import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    password_hash = db.Column(db.String(250))
