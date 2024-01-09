2d8aa24ba2c70efe4f64564dbb7a49f83ba23796
nine96as <ashrafsaid79@gmail.com>
2024-01-09 18:27:46 +0000

    Tweak init dunder func, check_pass, set_pass, json func

--- app/models.py
+++ app/models.py
@@ -1,7 +1,27 @@
 from .extensions import db
+from sqlalchemy.sql import func
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
     password_hash = db.Column(db.String(250))
+    created_at = db.mapped_column(db.DateTime, default=func.now())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def __repr__(self):
+        return f"User(id: {self.id}, username: {self.username})"
+
+    @property
+    def json(self):
+        return {"id": self.id, "username": self.username, "created_at": self.created_at}
