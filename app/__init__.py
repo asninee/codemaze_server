ced77ce2c6e6378a82e20b625e5fa4ffb6fc1a4b
nine96as <nine96@skiff.com>
2024-01-09 17:13:51 +0000

    Add User router refs, add jwt user lookup helper funcs

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from dotenv import load_dotenv
 
 from .extensions import api, db, jwt
+from .routers.users import userRouter
+from .models import User
 
 load_dotenv()
 
@@ -16,5 +18,16 @@ def create_app():
     api.init_app(app)
     db.init_app(app)
     jwt.init_app(app)
+
+    api.add_namespace(userRouter)
+
+    @jwt.user_identity_loader
+    def user_identity_lookup(user):
+        return user.id
+
+    @jwt.user_lookup_loader
+    def user_lookup_callback(_jwt_header, jwt_data):
+        identity = jwt_data["sub"]
+        return User.query.filter_by(id=identity).first()
 
     return app
