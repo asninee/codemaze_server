b714c74d0057a317151a86db8da0f070c91a060b
nine96as <ashrafsaid79@gmail.com>
2024-01-10 11:14:12 +0000

    Add Problem model test fixture

--- __tests__/conftest.py
+++ __tests__/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.models import User, Rank
+from app.models import User, Rank, Problem
 
 
 @pytest.fixture(scope="module")
@@ -13,3 +13,13 @@ def new_user():
 def new_rank():
     rank = Rank(name="Bronze", min_xp=0, max_xp=250)
     return rank
+
+
+@pytest.fixture(scope="module")
+def new_problem():
+    problem = Problem(
+        title="labore",
+        content="Commodo nostrud Lorem et deserunt commodo Lorem est officia reprehenderit sunt eiusmod Lorem ex amet. Mollit deserunt est amet aute cillum proident non ipsum deserunt nisi labore tempor irure non sunt. Sunt duis qui minim proident exercitation labore minim mollit aliquip fugiat anim. Est proident esse anim sint ut proident aute ullamco voluptate veniam dolore nulla. Do incididunt aliquip eu Lorem proident. Qui ad ullamco anim anim fugiat aliquip. Ut minim proident dolore.",
+        rank_id=2,
+    )
+    return problem
