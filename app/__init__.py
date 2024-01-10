2a9d7e0917dd23aaf0940010741eee7b9d008278
jarvis <jsd.ditcher@outlook.com>
2024-01-10 16:13:37 +0000

    small commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,27 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
-from flask_socketio import SocketIO
-from .extensions import api, db, jwt
+from .extensions import api, db, jwt, socketio
 from .routers.users import userRouter
 from .routers.sockets import sockets
 from .models import User
 
 load_dotenv()
 
-socketio =  SocketIO()
-
 def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["SQLALCHEMY_DATABASE_URI"]
     app.config["JWT_SECRET_KEY"] = os.environ["JWT_SECRET_KEY"]
+    app.config["SECRET_KEY"] = "secret"
 
     api.init_app(app)
     db.init_app(app)
     jwt.init_app(app)
+
+    app.register_blueprint(sockets)
+
+    socketio.init_app(app)
 
     api.add_namespace(userRouter)
 
@@ -32,15 +34,4 @@ def create_app():
         identity = jwt_data["sub"]
         return User.query.filter_by(id=identity).first()
     
-
-    @socketio.on("message")
-    def handle_message(msg):
-        print("Received message: " + msg)
-        socketio.emit("Message", msg, broadcast=True)
-
-
-    socketio.init_app(app)
-
-    app.register_blueprint(sockets)
-
     return app
