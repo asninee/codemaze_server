dae4c0ce3e33d96ea0291796750f9aebc5bdb1a0
nine96as <nine96@skiff.com>
2024-01-10 10:45:18 +0000

    Add Rank model test fixture

--- __tests__/conftest.py
+++ __tests__/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 
-from app.models import User
+from app.models import User, Rank
 
 
 @pytest.fixture(scope="module")
 def new_user():
     user = User(username="a", password="jkl")
     return user
+
+
+@pytest.fixture(scope="module")
+def new_rank():
+    rank = Rank(name="Bronze", min_xp=0, max_xp=250)
+    return rank
