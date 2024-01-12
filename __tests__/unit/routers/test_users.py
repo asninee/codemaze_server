1a513653cc79089b6aedea1a1f7cb9b292e6c2f0
nine96as <ashrafsaid79@gmail.com>
2024-01-12 12:08:23 +0000

    Add initial router testing stuffs

---
+++ __tests__/unit/routers/test_users.py
@@ -0,0 +1,8 @@
+from app.models import User
+
+
+# def test_register(client, app):
+#     response = client.post("/users/register", data={"username": "d", "password": "jkl"})
+
+# with app.app_context():
+#     assert User.query.count() == 4
