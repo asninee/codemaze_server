01a12ae1a38578ed4d25868b1f69410987e78f57
nine96as <ashrafsaid79@gmail.com>
2024-01-12 17:04:44 +0000

    Revamp sessionRouter logic to only use post request to submit session data

--- app/routers/sessions.py
+++ app/routers/sessions.py
@@ -6,8 +6,6 @@ from app.models import Session, User
 from app.api_models import (
     session_input_model,
     session_model,
-    session_update_model,
-    user_model,
 )
 
 
@@ -27,32 +25,37 @@ class SessionsAPI(Resource):
     @sessionRouter.expect(session_input_model)
     @sessionRouter.marshal_with(session_model)
     def post(self):
+        """Create a new session with a related problem, participants, and a winner assigned to it"""
         try:
-            session = Session(problem_id=sessionRouter.payload["problem_id"])
+            user1 = User.query.get(sessionRouter.payload["user_one_id"])
+            user2 = User.query.get(sessionRouter.payload["user_two_id"])
+            winner = User.query.get(sessionRouter.payload["winner_id"])
+
+            if not user1 and user2 and winner:
+                return {"error": "Either one or both Users do not exist"}, 404
+
+            session = Session(
+                problem_id=sessionRouter.payload["problem_id"], winner_id=winner.id
+            )
+
             db.session.add(session)
+            session.users.append(user1)
+            session.users.append(user2)
+
+            winner.xp += 10
+            winner.wins += 1
+
+            if winner.id != user1.id:
+                loser = user1
+            else:
+                loser = user2
+
+            loser.losses += 1
+
+            if winner.xp > winner.rank.max_xp:
+                winner.rank_up()
+
             db.session.commit()
             return session, 201
         except:
             return {"error": "We could not process your request"}, 400
-
-
-@sessionRouter.route("/<int:id>")
-class SessionAPI(Resource):
-    method_decorators = [jwt_required()]
-
-    @sessionRouter.doc(security="jsonWebToken")
-    @sessionRouter.expect(session_update_model)
-    @sessionRouter.marshal_with(session_model)
-    def patch(self, id):
-        session = Session.query.get(id)
-        user = User.query.get(sessionRouter.payload["user_id"])
-        if not session:
-            return {"error": "Session does not exist"}, 404
-        if not user:
-            return {"error": "User does not exist"}, 404
-
-        session.users.append(user)
-
-        db.session.commit()
-
-        return session, 200
