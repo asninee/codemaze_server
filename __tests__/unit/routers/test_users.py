65d652013af53a8e919228e0480eb185962439be
nine96as <ashrafsaid79@gmail.com>
2024-01-16 20:36:58 +0000

    Rm /users tests - changed to /auth

--- __tests__/unit/routers/test_users.py
+++
@@ -1,8 +0,0 @@
-from app.models import User
-
-
-# def test_register(client, app):
-#     response = client.post("/users/register", data={"username": "d", "password": "jkl"})
-
-# with app.app_context():
-#     assert User.query.count() == 4
