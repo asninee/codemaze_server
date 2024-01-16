4a7702d18b95696814e29e43aa0337f602fdeba6
nine96as <nine96@skiff.com>
2024-01-16 12:05:26 +0000

    Add test_case attr to Example model test

--- __tests__/unit/models/test_example.py
+++ __tests__/unit/models/test_example.py
@@ -10,4 +10,5 @@ def test_new_example_with_fixture(new_example):
         new_example.explanation
         == "Commodo exercitation in nulla aliqua reprehenderit magna reprehenderit adipisicing. Do pariatur consequat eu ad ut eu tempor elit. Eiusmod esse Lorem aliquip pariatur ea. Fugiat consectetur do est et magna labore sunt tempor quis."
     )
+    assert new_example.test_case == "occaecat do irure dolor"
     assert new_example.__repr__() == f"Example(problem_id: {new_example.problem_id})"
